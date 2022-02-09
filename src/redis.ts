c5b44048def351f7a043fce5098832403e5e4b5a
ayeolakenny <ayeolakenny@gmail.com>
2022-02-09 04:37:52 +0100

    fixes

--- src/redis.ts
+++ src/redis.ts
@@ -4,12 +4,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const redis = new Redis(process.env.REDIS_URL, {
-  tls: {
-    rejectUnauthorized: false,
-  },
+  // tls: {
+  //   rejectUnauthorized: false,
+  // },
+  db: 0,
   connectTimeout: 10000,
-  host: 'ec2-35-168-158-130.compute-1.amazonaws.com',
-  port: 23600,
-  password: 'p4ec7053ab10c67ab6b137198ad9a611dabff34b5cc82d1770db88a403d591776',
-  // url: 'redis://:p4ec7053ab10c67ab6b137198ad9a611dabff34b5cc82d1770db88a403d591776@ec2-35-168-158-130.compute-1.amazonaws.com:23600',
 });
