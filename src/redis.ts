f701c3ba2f785236d91b78a570648f01cccba2ac
ayeolakenny <ayeolakenny@gmail.com>
2022-02-17 21:09:24 +0100

    fixes

--- src/redis.ts
+++ src/redis.ts
@@ -3,24 +3,27 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-// export const redis = new Redis(process.env.REDIS_URL, {
-// tls: {
-//   rejectUnauthorized: false,
-// },
-//   db: 0,
-//   connectTimeout: 10000,
-// });
+export const redis = new Redis(
+  'redis-13951.c15.us-east-1-2.ec2.cloud.redislabs.com:13951',
+  {
+    tls: {
+      rejectUnauthorized: false,
+    },
+    db: 0,
+    connectTimeout: 10000,
+  },
+);
 
-const REDIS_URL = process.env.REDIS_URL;
-export const redis = REDIS_URL.includes('rediss://')
-  ? new Redis({
-      port: 23600,
-      host: 'ec2-35-168-158-130.compute-1.amazonaws.com',
-      password:
-        'p4ec7053ab10c67ab6b137198ad9a611dabff34b5cc82d1770db88a403d591776',
-      db: 0,
-      tls: {
-        rejectUnauthorized: false,
-      },
-    })
-  : new Redis(REDIS_URL);
+// const REDIS_URL = process.env.REDIS_URL;
+// export const redis = REDIS_URL.includes('rediss://')
+//   ? new Redis({
+//       port: 23600,
+//       host: 'ec2-35-168-158-130.compute-1.amazonaws.com',
+//       password:
+//         'p4ec7053ab10c67ab6b137198ad9a611dabff34b5cc82d1770db88a403d591776',
+//       db: 0,
+//       tls: {
+//         rejectUnauthorized: false,
+//       },
+//     })
+//   : new Redis(REDIS_URL);
