3da18279e873cf7f6b3797efbbe8ee4852315638
ayeolakenny <ayeolakenny@gmail.com>
2022-02-18 03:22:35 +0100

    fixes

--- src/redis.ts
+++ src/redis.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const redis = new Redis(process.env.REDISTOGO_URL, {
+export const redis = new Redis(process.env.REDIS_URL, {
   // tls: {
   //   rejectUnauthorized: false,
   // },
